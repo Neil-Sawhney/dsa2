45d9b8a5005a423268dba181c1737f67379ab047
Neil Sawhney <Neil@neil-sawhney.com>
2022-09-12 15:34:05 -0400

    IT WORKS

--- ProgAssign1_spellCheck/include/spellcheck.h
+++ ProgAssign1_spellCheck/include/spellcheck.h
@@ -1,16 +1,29 @@
 #pragma once
-
 #include "../include/hash.h"
+
 // returns true if the passed in letter is a word seperator
 bool isWordSep(char letter);
 
+// return VALID if the word is valid
+// return CONTAIN_NUMBERS if the word is valid but contains numbers
 // return TOO_LONG if the word is >= 20 characters
 // return BAD_CHAR if the word contains a non-alphanumeric character
-// return VALID if the word is valid
-enum invalidReason {VALID, TOO_LONG, BAD_CHAR};
-invalidReason wordValidity(std::string word);
+// return EMPTY if the word is empty
+enum wordType {VALID, CONTAINS_NUMBERS, TOO_LONG, BAD_CHAR, EMPTY};
+wordType chkWord(std::string word);
 
 // loads the dictionary into the hash table
 // converts all words to lowercase
-void loadDictionary(hashTable &dictionary, std::string filename);
+// prints the cpu time to the standard output
+void loadDictionary(hashTable &dictionary, std::string &filename);
 
+// prints to the output file any unrecognized words along with their line number
+// If a word is too long, indicate that, print the first 20 characters and the line number
+// The first line in the document is line 1
+// Ignores any words that contain numbers
+// prints the cpu time to the standard output
+void spellcheck(hashTable &dictionary, std::string &spellcheckFile, std::string &outputFile);
+
+
+// checks if the word is valid, and if its too long, and prints the appropriate message
+void checkAndPrint(hashTable &dictionary ,std::string word, int lineNum, std::ofstream &output);
--- ProgAssign1_spellCheck/src/main.cpp
+++ ProgAssign1_spellCheck/src/main.cpp
@@ -8,7 +8,21 @@ int main()
     std::cout << "Enter the dictionary file: ";
     std::cin >> dictionaryFile;
 
+    std::string spellcheckFile;
+    std::cout << "Enter the file to be spell checked: ";
+    std::cin >> spellcheckFile;
+
+    std::string outputFile;
+    std::cout << "Enter the output file: ";
+    std::cin >> outputFile;
+
+    // TEMPORARY
+    dictionaryFile = "./sample_input/wordlist_small";
+    spellcheckFile = "./sample_input/lyrics.txt";
+    outputFile = "./output/lyricsSpellchecked.txt";
+
     hashTable dictionary(50000);
     loadDictionary(dictionary, dictionaryFile);
+    spellcheck(dictionary, spellcheckFile, outputFile);
 
 }
